608296549843a236449cfc4b76fd592b26460048
katia9116 <st9097574@stud.duikt.edu.ua>
2025-12-12 13:20:38 +0200

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,30 @@
+﻿// 2.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Enter (x, y): ";
+    cin >> x >> y;
+
+    bool inCircle = (x <= 0 && y <= 0 && (x * x + y * y <= 1));
+    bool inTriangle = (x >= 0 && y >= 0 && y <= -x + 1);
+
+    if (inCircle || inTriangle)
+        cout << "Point (" << x << ", " << y << ") belongs to the shaded area." << endl;
+    else
+        cout << "Point (" << x << ", " << y << ") not belongs to the shaded area." << endl;
+
+    return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
